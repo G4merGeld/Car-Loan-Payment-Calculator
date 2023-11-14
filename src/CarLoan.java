bd72e8b624260e56f6407b0bccea8cc21b6942e6
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-14 20:36:47 +0100

    added method to check if loan values make sense

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -1,2 +1,19 @@
 public class CarLoan {
+    public static void main(String[] args) {
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+        checkLoan(loanLength, interestRate);
+    }
+
+    public static boolean checkLoan(int loanLength, int interest){
+        if((loanLength <= 0) || (interest <= 0)) {
+            System.out.println("Error: Loan not possible");
+            return false;
+        } else {
+            System.out.println("Loan acceptable");
+            return true;
+        }
+    }
 }
